27273d261705265a88da6bf38e12a0577a55a4a2
CursoNET2017 <pruebaCurso2017@hotmail.com>
2017-09-08 15:55:37 +0200

    Agregar archivos de proyecto.

---
+++ IndividualServidorApuestas/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace IndividualServidorApuestas.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ IndividualServidorApuestas/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace IndividualServidorApuestas.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
